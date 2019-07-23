6128500b27a3b5a43cca280cc2bbfee429571b4b
mujawamariya <mumarg2015@gmail.com>
2019-07-23 05:26:07 -0700

    my calculator

---
+++ calculator.js
@@ -0,0 +1,114 @@
+
+var number1,number2,result;
+
+
+
+
+myfunction1=()=>{
+document.getElementById("textdisplay").value +="1"
+}
+
+
+myfunction2=()=>{
+    document.getElementById("textdisplay").value +="2"
+    }
+
+    
+myfunction3=()=>{
+    document.getElementById("textdisplay").value +="3"
+    }
+
+    
+myfunction4=()=>{
+    document.getElementById("textdisplay").value +="4"
+    }
+    
+myfunction5=()=>{
+    document.getElementById("textdisplay").value +="5"
+    }
+    
+myfunction6=()=>{
+    document.getElementById("textdisplay").value +="6"
+    }
+    
+myfunction7=()=>{
+    document.getElementById("textdisplay").value +="7"
+    }
+    
+myfunction8=()=>{
+    document.getElementById("textdisplay").value +="8"
+    }
+    
+myfunction9=()=>{
+    document.getElementById("textdisplay").value +="9"
+    }
+    
+myfunction10=()=>{
+    document.getElementById("textdisplay").value +="0"
+    }
+    
+myfunction11=()=>{
+    document.getElementById("textdisplay").value +="%"
+    result=numbre1 % number2;
+    result=document.getElementById("textdisplay")
+    }
+    
+myfunction12=()=>{
+    document.getElementById("textdisplay").value +="*"
+    result=numbre1 * number2;
+    result=document.getElementById("textdisplay")
+    }
+    
+    myfunction13=()=>{
+        document.getElementById("textdisplay").value +="."
+        }
+    
+        myfunction14=()=>{
+            document.getElementById("textdisplay").value =" "
+            
+            }
+
+    
+            myfunction21=()=>{
+                document.getElementById("textdisplay").value +="+" 
+                result=number1 + number2;
+    result=document.getElementById("textdisplay")
+                
+                }
+        
+            myfunction15=()=>{
+                document.getElementById("textdisplay").value +="/"
+                result=numbre1 / number2;
+    result=document.getElementById("textdisplay")
+                }
+    
+                myfunction16=()=>{
+                    document.getElementById("textdisplay").value = eval(Math.sqrt(textdisplay.value))
+                    }
+                    myfunction22=()=>{
+                        document.getElementById("textdisplay").value = eval((textdisplay.value) * (textdisplay.value))
+                        }
+                        
+    
+    
+                        myfunction19=()=>{
+                        
+                            document.getElementById("textdisplay").value +="-"
+                            result=numbre1 - number2;
+    result=document.getElementById("textdisplay")
+                            }
+                            
+
+                            myfunction20=()=>{
+                          const a=textdisplay
+                          const b=document.getElementById("equal")
+
+                          document.getElementById("textdisplay").value = eval(textdisplay.value) 
+                                
+                                
+                                }
+                        
+                        myfunction20.click=()=>{
+                            const c =  document.getElementById("textdisplay").value 
+                            document.getElementById("textdisplay").value += c     
+                        }                  
